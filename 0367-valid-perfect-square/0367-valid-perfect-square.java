7512c969aac5893f2d69c32d9a78f802765101a1
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-09 18:25:00 +0530

    Time: 1 ms (20.37%), Space: 40.6 MB (18.09%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -3,7 +3,10 @@ class Solution {
         int i=1;
         while(num > 0)
         {
-            
+            // if num - i = 0 squre exist
+            //Repeatedly subtracting consecutive odd numbers from it
+            //Subtract till the difference is zero
+            //Number of times we subtract is the required square root
             num = num - i;
             i=i+2;
         }
