939555b18505f0616cff5dd87a131347029afea5
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-10 16:51:44 +0530

    Update 0258-add-digits.java

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,18 +1,13 @@
 
 class Solution {
     public int addDigits(int num) {
-        String s = Integer.toString(num);
-        int r = 0;
-        if (s.length() == 1)
-        {
-            return Integer.parseInt(s);
+        if (num == 0) {
+            return 0;
         }
-        char a[] = s.toCharArray();
-            for(int i=0;i<a.length;i++)
-            {
-                r = r+ Character.getNumericValue(a[i]);
-            }
-        return addDigits(r);
-       
+        // mathematical property of digital roots
+        if (num % 9 == 0) {
+            return 9;
+        }
+        return num % 9;
     }
 }
