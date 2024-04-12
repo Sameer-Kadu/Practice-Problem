36df95379a317cec6d849ed4f641afd87de58817
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-12 16:24:16 +0530

    Time: 2 ms (85.14%), Space: 46.3 MB (20.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,23 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       
-        
-          int left = 0;
-        int right = 0;
-
-        // Iterate through the array
-        while (right < nums.length) {
-            // If the current element is not zero, swap it with the left pointer
-            if (nums[right] != 0) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-            }
-            
-            right++; 
-            
+       int c=0;
+       for(int i =0;i<nums.length;i++)
+       {
+        if(nums[i]!=0)
+        {
+            nums[c++]=nums[i];
         }
+       }
+       while(c<nums.length)
+       {
+        nums[c++]=0;
+       }
+            
+        
         
     }
 }
