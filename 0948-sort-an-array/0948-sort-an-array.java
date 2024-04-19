ef14ec564af657672fc23e1961a67591626540f1
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-19 19:03:07 +0530

    Time: 29 ms (45.05%), Space: 56.7 MB (29.49%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+    
+    private void sort(int[] nums, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start) / 2;
+            sort(nums, start, mid);
+            sort(nums, mid + 1, end);
+            merge(nums, start, mid, end);
+        }
+    }
+    
+    private void merge(int[] nums, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+        
+        while (i <= mid && j <= end) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+        
+        while (i <= mid) { 
+            temp[k++] = nums[i++];
+        }
+        
+        while (j <= end) {
+            temp[k++] = nums[j++];
+        }
+        
+        for (i = start; i <= end; i++) {
+            nums[i] = temp[i - start];
+        }
+    }
+}
