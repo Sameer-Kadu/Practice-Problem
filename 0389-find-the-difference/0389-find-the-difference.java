544e762157542532d45ac697b8eabd609e6827c6
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-22 02:26:07 +0530

    Update 0389-find-the-difference.java

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,18 +1,25 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char a[]= s.toCharArray();
-        char b[]= t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int i=0;
-        while(i<s.length())
-        {
-            if(a[i]!=b[i])
-            {
-                return b[i];
-            }
-            i++;
+        int[] charCount = new int[26]; // Assuming input consists only of lowercase English letters
+        
+        // Count occurrences of each character in string s
+        for (char c : s.toCharArray()) {
+            charCount[c - 'a']++;
         }
-        return b[b.length-1];
+        
+        // Subtract occurrences of characters in string t
+        for (char c : t.toCharArray()) {
+            charCount[c - 'a']--;
+        }
+        
+        // Find the character with non-zero count
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] != 0) {
+                return (char) (i + 'a');
+            }
+        }
+        
+        // In case input strings are not valid or there's no difference
+        return '\0';
     }
 }
