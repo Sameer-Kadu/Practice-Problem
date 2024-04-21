9ed6f12dc097c5771b9ab558e163ed0f5db0310e
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2024-04-21 18:27:32 +0530

    Time: 1 ms (99.34%), Space: 45.4 MB (10.92%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> l = new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            if(i<3)
+            {
+                l.add(Integer.toString(i));
+            }
+            else if(i==3)
+            {
+                l.add("Fizz");
+            }
+            else if(i<5)
+            {
+                l.add(Integer.toString(i));
+            }
+            else if(i==5)
+            {
+                l.add("Buzz");
+            }
+            else if(i%3==0 && i%5==0)
+            {
+                l.add("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                l.add("Fizz");
+            }
+            else if(i%5==0)
+            {
+                l.add("Buzz");
+            }
+            else
+            {
+                l.add(Integer.toString(i));
+            }
+        }
+        return l;
+    }
+}
