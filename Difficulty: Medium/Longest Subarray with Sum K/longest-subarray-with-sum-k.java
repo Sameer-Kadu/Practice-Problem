593dbecef323cc6ba5867b4e1f8fa792c6cf5f9d
Sameer Kadu <115989281+Sameer-Kadu@users.noreply.github.com>
2025-02-21 11:45:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution();
+            int ans = ob.longestSubarray(nums, k); // Call the function and store result
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        int maxLength = 0;
+        int sumK = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            
+                sumK += arr[i];
+                 if (sumK == k) {
+                maxLength = i + 1;
+            }
+                
+                
+                int temp = sumK - k;
+                if(map.containsKey(temp))
+                {
+                    maxLength = Math.max(maxLength, i - map.get(temp) );
+                }
+                map.putIfAbsent(sumK, i);
+            
+        }  
+        return maxLength;
+    }
+}
